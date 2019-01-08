bee085bf6b464dba048e8b68fabc7c3fa6a92cb9
yanchao <yanchao.37@163.com>
2019-01-08 12:00:41 +0800

    CRLF --> LF

--- bin/sh/pre-release.sh
+++ bin/sh/pre-release.sh
@@ -1,1 +1,22 @@
-#!/usr/bin/env shgit checkout devif test -n "$(git status --porcelain)"; then  echo -e '\E[1;31mRelease Error: Unclean working tree. Commit or stash changes first.\E[0m' >&2;  exit 128;fiif ! git fetch --quiet 2>/dev/null; then  echo -e '\E[1;31mRelease Error:  There was a problem fetching your branch. Run `git fetch` to see more...\E[0m' >&2;  exit 128;fiif test "0" != "$(git rev-list --count --left-only @'{u}'...HEAD)"; then  echo -e '\E[1;31mRelease Error:  Remote history differ. Please pull changes.\E[0m' >&2;  exit 128;fiecho 'No conflicts.' >&2;git checkout mastergit merge dev
+#!/usr/bin/env sh
+git checkout dev
+
+if test -n "$(git status --porcelain)"; then
+  echo -e '\E[1;31mRelease Error: Unclean working tree. Commit or stash changes first.\E[0m' >&2;
+  exit 128;
+fi
+
+if ! git fetch --quiet 2>/dev/null; then
+  echo -e '\E[1;31mRelease Error:  There was a problem fetching your branch. Run `git fetch` to see more...\E[0m' >&2;
+  exit 128;
+fi
+
+if test "0" != "$(git rev-list --count --left-only @'{u}'...HEAD)"; then
+  echo -e '\E[1;31mRelease Error:  Remote history differ. Please pull changes.\E[0m' >&2;
+  exit 128;
+fi
+
+echo 'No conflicts.' >&2;
+
+git checkout master
+git merge dev
