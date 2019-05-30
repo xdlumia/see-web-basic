05835fcea4885e0519b1ea322874492b7c1dc3b9
晓冬 <xdq@live.cn>
2019-05-30 18:27:39 +0800

    修改服务器时间

--- src/utils/serversDate.js
+++ src/utils/serversDate.js
@@ -8,22 +8,23 @@
 
 import axios from 'axios';
 
-let apiUrl = window.g.ApiUrl
-function getServersDate(){
-  axios.get(apiUrl.bizSystemService + '/serve/getServerTime').then(res=>{
-    Date.serversDate = function () { return new Date(res.data || new Date()) }
-  })
+var apiUrl = window.g.ApiUrl;
+let t = new Date()
+function getServersDate() {
+  axios.get(apiUrl.bizSystemService + '/serve/getServerTime').then(function (res) {
+    t = res.data || new Date()
+  });
 }
+Date.serversDate = function () {
+  return new Date(t)
+};
 // 获取服务器时间
-getServersDate()
+getServersDate();
 // 给服务器时间每隔1秒递增1秒时间
-let timer = setInterval(function(){
-  let newDate = Date.serversDate()
-  Date.serversDate = function () {
-    return new Date(newDate.valueOf() + 1000)
-  }
-},1000)
+var timer = setInterval(function () {
+  t += 1000
+}, 1000);
 // 每隔30秒重新获取一次服务器时间
-let serverTimer = setInterval(function(){
-  getServersDate()
-},30000)
+var serverTimer = setInterval(function () {
+  getServersDate();
+}, 30000);
