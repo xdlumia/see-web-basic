b48b66328ea0d06bed9aca38346d86a5a7915e26
晓冬 <xdq@live.cn>
2019-06-02 19:14:46 +0800

    修改服务时间调取方法

--- src/utils/serversDate.js
+++ src/utils/serversDate.js
@@ -9,22 +9,28 @@
 import axios from 'axios';
 
 var apiUrl = window.g.ApiUrl;
-let t = new Date()
-function getServersDate() {
-  axios.get(apiUrl.bizSystemService + '/serve/getServerTime').then(function (res) {
-    t = res.data || new Date()
+var t = new Date();
+// function getServersDate() {
+//   axios.get(apiUrl.bizSystemService + '/serve/getServerTime').then(function (res) {
+//     t = res.data || new Date();
+//   });
+// }
+Date.serversDate = async function () {
+  await axios.get(apiUrl.bizSystemService + '/serve/getServerTime').then(function (res) {
+    t = res.data || new Date(); 
   });
-}
-Date.serversDate = function () {
-  return new Date(t)
+  return new Date(t);
 };
-// 获取服务器时间
-getServersDate();
-// 给服务器时间每隔1秒递增1秒时间
-var timer = setInterval(function () {
-  t += 1000
-}, 1000);
-// 每隔30秒重新获取一次服务器时间
-var serverTimer = setInterval(function () {
-  getServersDate();
-}, 30000);
+// Date.serversDate = function () {
+//   return new Date(t)
+// };
+// // 获取服务器时间
+// getServersDate();
+// // 给服务器时间每隔1秒递增1秒时间
+// var timer = setInterval(function () {
+//   t += 1000;
+// }, 1000);
+// // 每隔30秒重新获取一次服务器时间
+// var serverTimer = setInterval(function () {
+//   getServersDate();
+// }, 30000);
